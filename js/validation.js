787ad58de309c00c3c7777aa0622ad379e6c1297
quadrovicente28 <118233540+quadrovicente28@users.noreply.github.com>
2023-01-24 14:44:47 +0800

    deleted file email.js

--- js/validation.js
+++ js/validation.js
@@ -45,7 +45,6 @@ emailjs.send(serviceID,templateID,params)
     res =>{
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
-        console.log(res);
         alert(`Your message sent succesfully`);
     }
 )
